cd9f6c35a2ec8e044822fcbf15b42d47f35dd665
James Murphy <jamesivdeveloper@gmail.com>
2022-12-10 17:20:31 -0600

    add functionality to fetch joke from joke api

--- script.js
+++ script.js
@@ -105,17 +105,40 @@ const VoiceRSS = {
   },
 };
 
-function test() {
-  VoiceRSS.speech({
-    key: "d5ca33a368e14e4ab2f5c863fd5bda38",
-    src: "Hello, world!",
-    hl: "en-us",
-    v: "Linda",
-    r: 0,
-    c: "mp3",
-    f: "44khz_16bit_stereo",
-    ssml: false,
-  });
-}
+// function test() {
+//   VoiceRSS.speech({
+//     key: "d5ca33a368e14e4ab2f5c863fd5bda38",
+//     src: "Hello, world!",
+//     hl: "en-us",
+//     v: "Linda",
+//     r: 0,
+//     c: "mp3",
+//     f: "44khz_16bit_stereo",
+//     ssml: false,
+//   });
+// }
 
-test();
+// test();
+
+// get jokes from Joke API
+
+async function getJokes() {
+  let joke = "";
+  const apiUrl =
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    console.log(joke);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getJokes();
